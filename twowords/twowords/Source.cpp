d07d922d238629e50d5b07cdb61eb3e7c6793d9e
benco <bcorn@bu.edu>
2016-02-25 20:11:41 -0500

    Update

--- twowords/twowords/Source.cpp
+++ twowords/twowords/Source.cpp
@@ -1,3 +1,17 @@
+//============================================================================
+// Name        : twowords.cpp
+// Author      : Benjamin Corn, Ryan Morano
+// Version     : 0.00
+// Copyright   : Boston University
+// Description : Game
+//============================================================================
+
+//============================================================================
+// AUTHOR1: Benjamin Corn bcorn@bu.edu
+// AUTHOR2: Ryan Morano rmorano@bu.edu
+// AUTHOR3:
+//============================================================================
+
 #include <iostream>
 #include <vector>
 #include <sstream>
