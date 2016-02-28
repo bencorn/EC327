51d5f7eb8f07f344ceaaab9dba8348c47dd4da02
Benjamin Corn <bcorn@bu.edu>
2016-02-27 19:23:56 -0500

    Up to date version

--- twowords/twowords/Source.cpp
+++ twowords/twowords/Source.cpp
@@ -21,158 +21,157 @@
 #include <map>
 
 using namespace std;
-
-vector <string> fileopen(int length);
-
-
-
-void maskgen(void);
-
+vector <int> returnwords(vector <string>, vector <string>);
+vector <string> elimnfind(vector <string>, vector<int>);
+void maskgen();
 
 int main() {
-    
-    string default_letters = "aadekmmnortww";
-    vector <string> letters;
-    
-    void maskgen();
-    
-    
-    
-    
-    
-    for (int i = 1; i < default_letters.length(); i++)
-        
-        }
+	
+	cout << "Enter letter collection" << endl;
+	string input;
+	cin >> input;
 
+	string binletters = letters2bits(input);
 
-
-void maskgen() {
-    
-    vector<string>
-    
-    //first create the map of the letters that we can use as indices
-    map<string, int> bindict= {
-        {"a", 0},
-        {"b", 0},
-        {"c", 0},
-        {"d", 0},
-        {"e", 0},
-        {"f", 0},
-        {"g", 0},
-        {"h", 0},
-        {"i", 0},
-        {"j", 0},
-        {"k", 0},
-        {"l", 0},
-        {"m", 0},
-        {"n", 0},
-        {"o", 0},
-        {"p", 0},
-        {"q", 0},
-        {"r", 0},
-        {"s", 0},
-        {"t", 0},
-        {"u", 0},
-        {"v", 0},
-        {"w", 0},
-        {"x", 0},
-        {"y", 0},
-        {"z", 0}
-    };
-    
-    ofstream bitfile;
-    bitfile.open("bit_words.txt")
-    
-    string line;
-    ifstream myfile("wordlist.txt");
-    if (myfile.is_open())
-    {
-        while (getline(myfile, line))
-        {
-            temp_bin_dict = bindict;
-            string bit_mask = "";
-            for (i = 0; i < line.length(); i++){
-                temp_bin_dict[line[i]] = 1;
-                
-                for (auto& kv: bindict){
-                    bit_mask = bit_mask + kv.second
-                }
-                
-                
-            }
-            bitfile << bit_mask
-        }
-        myfile.close();
-        bitfile.close();
-    }
-    
-    
-    else cout << "Unable to open file";
-    return words;
-    
+	return 0;
 }
 
+void maskgen() {
+
+		//first create the map of the letters that we can use as indices
+		map<string, int> bindict = {
+			{ "a", 0 },
+			{ "b", 0 },
+			{ "c", 0 },
+			{ "d", 0 },
+			{ "e", 0 },
+			{ "f", 0 },
+			{ "g", 0 },
+			{ "h", 0 },
+			{ "i", 0 },
+			{ "j", 0 },
+			{ "k", 0 },
+			{ "l", 0 },
+			{ "m", 0 },
+			{ "n", 0 },
+			{ "o", 0 },
+			{ "p", 0 },
+			{ "q", 0 },
+			{ "r", 0 },
+			{ "s", 0 },
+			{ "t", 0 },
+			{ "u", 0 },
+			{ "v", 0 },
+			{ "w", 0 },
+			{ "x", 0 },
+			{ "y", 0 },
+			{ "z", 0 }
+	};
+
+	string line;
+	ifstream myfile("wordlist.txt");
+	if (myfile.is_open())
+	{
+		vector <string> bitvec;
+		while (getline(myfile, line))
+		{
+			map<string, int> temp_bin_dict = bindict;
+			string bit_mask = "";
+			string temp_key;
+			
+			for (int i = 0; i < line.length(); i++) {
+				temp_key = line[i];
+				temp_bin_dict[temp_key] = 1;
+
+				
+				}
+
+			for (auto& kv : temp_bin_dict) {
+			bit_mask = bit_mask + to_string(kv.second);
+			
+			}
+			bitvec.push_back(bit_mask);
+		}
+		myfile.close();
+
+		ofstream bitfile;
+		bitfile.open("bit_words.txt");
+
+		for (int i = 0; i < bitvec.size(); i++) {
+			bitfile << bitvec[i] << endl;
+		}
+		bitfile.close();
+	}
 
 
+	else cout << "Unable to open file";
+
+}
+
+vector <int> returnwords(vector <string> letters, vector <string> bit_words) {
+	vector <int> indices;
+	for (int i = 0; i < bit_words.size(); i++) {
+		if (letters[i] == bit_words[i]) {
+			indices.push_back(i);
+		}
+	}
+	return indices;
+
+}
+
+vector <string> elimnfind(vector <string> word_list, vector<int> indices, int length_param) {
+	vector <string> word_matches;
+	for (int i = 0; i < indices.size(); i++) {
+		if (word_list[indices[i]].length() == length_param) {
+			word_matches.push_back(word_list[indices[i]]);
+		}
+
+	}
+}
+
+string letters2bits(string input) {
+	map<string, int> bindict = {
+		{ "a", 0 },
+		{ "b", 0 },
+		{ "c", 0 },
+		{ "d", 0 },
+		{ "e", 0 },
+		{ "f", 0 },
+		{ "g", 0 },
+		{ "h", 0 },
+		{ "i", 0 },
+		{ "j", 0 },
+		{ "k", 0 },
+		{ "l", 0 },
+		{ "m", 0 },
+		{ "n", 0 },
+		{ "o", 0 },
+		{ "p", 0 },
+		{ "q", 0 },
+		{ "r", 0 },
+		{ "s", 0 },
+		{ "t", 0 },
+		{ "u", 0 },
+		{ "v", 0 },
+		{ "w", 0 },
+		{ "x", 0 },
+		{ "y", 0 },
+		{ "z", 0 }
+	};
+	map<string, int> temp_bin_dict = bindict;
+	string bit_mask = "";
+	string temp_key;
+
+	for (int i = 0; i < input.length(); i++) {
+		temp_key = input[i];
+		temp_bin_dict[temp_key] = 1;
 
 
+	}
 
+	for (auto& kv : temp_bin_dict) {
+		bit_mask = bit_mask + to_string(kv.second);
 
-
-
-//
-//    // load the "twowords" text file
-//
-//
-//    void maskgen(int length) {
-//        vector <string> words;
-//        string line;
-//        ifstream myfile("wordlist.txt");
-//            while (getline(myfile, line))
-//            {
-//                string temp_word;
-//                temp_word = line;
-//
-//            }
-//
-//
-//        ofstream newfile;
-//        newfile.open("bit_words.txt")
-//        newfile << words;
-//
-//
-//
-//
-//        myfile.close();
-//        newfile.close();
-//    }
-//
-//
-//
-//
-//}
-//
-//
-//
-//
-//
-//vector <string> fileopen(int length) {
-//	vector <string> words;
-//	string line;
-//	ifstream myfile("wordlist.txt");
-//	if (myfile.is_open())
-//	{
-//		while (getline(myfile, line))
-//		{
-//			string temp_word;
-//			temp_word = line;
-//			if (temp_word.length() > length)
-//				words.push_back(line);
-//		}
-//		myfile.close();
-//	}
-//
-//
-//	else cout << "Unable to open file";
-//	return words;
-//}
+	}
+	return bit_mask;
+}
